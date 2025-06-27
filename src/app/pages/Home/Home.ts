8b345816df6de00a977c56465419ac7756744fec
Mariem-9 <mariem.benamor001@gmail.com>
2025-06-27 14:46:52 +0100

    feature/TSHREF-001_gestion_groupe_front

--- src/app/pages/Home/Home.ts
+++ src/app/pages/Home/Home.ts
@@ -1,28 +1,19 @@
 import { Component } from '@angular/core';
-import { NotificationsWidget } from './components/notificationswidget';
-import { RecentSalesWidget } from './components/recentsaleswidget';
-import { BestSellingWidget } from './components/bestsellingwidget';
-import { RevenueStreamWidget } from './components/revenuestreamwidget';
-import { Timelinewidget } from './components/timelinewidget';
-import { Treewidget } from './components/treewidget';
-import { ButtonWidget } from "./components/appbuttonwidget";
-
-
+import { NestedMenuComponent } from '../../components/nested-menu-items.component';
 @Component({
     selector: 'app-home',
-    imports: [Treewidget, Timelinewidget, ButtonWidget],
+    imports: [NestedMenuComponent],
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <!-- <app-stats-widget class="contents" /> -->
             <div class="col-span-12 xl:col-span-6">
-                <app-button-widget/>
-                <app-timeline-widget />
-                <!-- <app-best-selling-widget /> -->
+                <!-- <app-timeline-widget /> -->
             </div>
             <div class="col-span-12 xl:col-span-6">
-                <app-tree-widget />
+                <app-nested-menu/>
+                <!-- <app-tree-widget /> -->
             </div>
         </div>
     `
 })
-export class Home {}
+export class Home {
+}
